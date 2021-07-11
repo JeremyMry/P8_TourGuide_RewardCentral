900653e061436d81ecc6455c102476538fc0122f
Jérémy MORY <jmory@divalto.com>
2021-07-11 13:07:59 +0200

    refactor

--- src/main/java/com/tourguide/rewardCentral/service/RewardCentralService.java
+++ src/main/java/com/tourguide/rewardCentral/service/RewardCentralService.java
@@ -1,5 +1,7 @@
 package com.tourguide.rewardCentral.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -9,6 +11,9 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class RewardCentralService {
 
+
+    private Logger logger = LoggerFactory.getLogger(RewardCentralService.class);
+
     public RewardCentralService() { }
 
     public int getAttractionRewardPoints(UUID attractionId, UUID userId) {
@@ -17,6 +22,9 @@ public class RewardCentralService {
         } catch (InterruptedException var4) {
         }
         int randomInt = ThreadLocalRandom.current().nextInt(1, 1000);
+
+        logger.debug("test");
+
         return randomInt;
     }
 }
